a05a61c7700ad20a8ef427806e9c6b7687efaa7d
AZorenkoV <azorenko@gmail.com>
2015-04-22 13:02:56 +0300

    Add Lessons

---
+++ src/Lesson02/zadanie01.java
@@ -0,0 +1,34 @@
+package Lesson02;
+
+public class zadanie01 {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 3;
+
+        System.out.println("Simple arithmetic operation");
+        System.out.println("x + y = " + (x + y) );
+        System.out.println("x - y = " + (x - y) );
+        System.out.println("x * y = " + (x * y) );
+        System.out.println("x / y = " + (x / y) );
+        System.out.println("x % y = " + (x % y) );
+
+        System.out.println();
+
+        System.out.println("Increment operation");
+        System.out.println("x = " + x);
+        System.out.println("++x = " + ++x);
+        System.out.println("x = " + x);
+        System.out.println("x++ = " + x++);
+        System.out.println("x = " + x);
+
+        System.out.println();
+
+        System.out.println("Decrement operation");
+        System.out.println("y = " + y);
+        System.out.println("--y = " + --y);
+        System.out.println("y = " + y);
+        System.out.println("y-- = " + y--);
+        System.out.println("y = " + y);
+
+    }
+}
---
+++ src/Lesson03/lesson03.java
@@ -0,0 +1,23 @@
+package Lesson03;
+
+import java.util.Scanner;
+
+/**
+ * Created by Raccoon on 20.04.2015.
+ */
+public class lesson03 {
+
+    public static final int INT = 100;
+
+    public static void main(String[] args) {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        System.out.print("Enter int: ");
+//        int x = scanner.nextInt();
+//
+//        System.out.println(x);
+          int w = INT;
+
+        System.out.println("Hello");
+    }
+}
